c9668069123b62ac6bcb1cce52b44542869d07a6
pelayord76 <pelayord76@educastur.es>
2023-10-04 09:40:09 +0200

    retoque variables

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-var arrayGrande = new Array(50);
+const size = 50;
+const defaultValue = 0;
+
+var arrayGrande = new Array(size);
 
 console.log(arrayGrande.fill(0));
 console.log(llenarArray(arrayGrande));
 
-function llenarArray(array){
-    for(let i = 0; i < array.length; i++){
-        array[i] = 0;
+function llenarArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = defaultValue;
     }
     return array;
 }
